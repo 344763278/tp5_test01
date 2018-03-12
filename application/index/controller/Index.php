4d9a15b2e32128000f1e24d88ec3a1b3b8f41069
tianyu <344763278@qq.com>
2018-03-13 00:38:31 +0800

    增删改查

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -25,21 +25,69 @@ class Index
     	}  
     }
 
-    public function getUserInfo()
+    public function getData()
     {
         $request = Request::instance();
         // dump($request->param('a'));
         // dump($request);
         // exit;
-        $user = Db::table('user')->select();
+        $user = Db::table('data_table')->select();
         // dump($user); 
         // exit;
         $data = array(
-            'code' => '1',
-            'msg'  => '失败',
+            'code' => '0',
+            'msg'  => '成功',
             'data' => $user
         );
     	return $data;  
         // echo Db::query('select * from user where id=1');
+    }
+
+    public function search()
+    {
+        $request = Request::instance(); 
+        $name = $request->param('name'); 
+        $data = Db::query('select * from data_table where name="'.$name.'"'); 
+        // $data = Db::table('data_table')->where('name',$name)->find();
+        dump($data); 
+         
+        return $this->sendMsg($data);    
+    }
+
+    public function del()
+    {
+        $request = Request::instance(); 
+        $id = $request->param('id'); 
+        $data = Db::query('delete from data_table where id="'.$id.'"'); 
+        //找id然后删除 
+        // $res = Db::table('data_table')->where('id',$id)->delete();
+        // dump($res);  
+        return $this->sendMsg($data);    
+    }
+
+    public function add()
+    {
+        $request = Request::instance(); 
+        $time = $request->param('time'); 
+        $name = $request->param('name'); 
+        $address = $request->param('address'); 
+
+        $data = ['time' => $time, 'name' => $name, 'address' => $address, 'id' => '9', 'status' => '0'];
+        var_dump($data);  
+        $res = Db('data_table') -> insert($data);
+        if ($res) {
+            return $this->sendMsg([],'0', '添加成功');
+        } else {
+            return $this->sendMsg([],'1', '添加失败');
+        }    
+    }
+
+    public function sendMsg($data=[], $code='0', $msg='成功')
+    {
+        return array(
+            'code' => $code,
+            'msg'  => $msg,
+            'data' => $data
+        );
     }
 }
